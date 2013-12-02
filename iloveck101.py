e40b0f6c87cb9c3d1600dfd55ea012520f487d84
tzangms <tzangms@gmail.com>
2013-12-02 21:04:14 +0800

    add comments

--- iloveck101.py
+++ iloveck101.py
@@ -9,9 +9,11 @@ from lxml import etree
 
 url = sys.argv[1]
 
+# find thread id
 m = re.match('thread-(\d+)-.*', url.rsplit('/', 1)[1])
 thread_id = m.group(1)
 
+# create required folders
 if not os.path.exists('images'):
     os.mkdir('images')
 
@@ -19,23 +21,27 @@ folder = os.path.join('images', thread_id)
 if not os.path.exists(folder):
     os.mkdir(folder)
 
-
+# fetch html and find images
 resp = requests.get(url)
 html = etree.HTML(resp.content)
 image_urls = html.xpath('//img/@file')
 
+# save images
 for image_url in image_urls:
     filename = image_url.rsplit('/', 1)[1]
 
+    # ignore useless image
     try:
         resp = requests.get(image_url)
     except requests.exceptions.MissingSchema:
         continue
 
+    # ignore small images
     im = Image.open(StringIO(resp.content))
     width, height = im.size
     if width < 400 or height < 400:
         continue
 
+    # save image
     with open(os.path.join(folder, filename), 'wb+') as f:
         f.write(resp.content)
