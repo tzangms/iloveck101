1729d9f607b9e6c191373dde9ca360f0db27946d
tzangms <tzangms@gmail.com>
2013-12-04 16:44:42 +0800

    Bump to v0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iloveck101',
-    version='0.4.0',
+    version='0.5.0',
     author='tzangms',
     author_email='tzangms@gmail.com',
     packages=['iloveck101'],
