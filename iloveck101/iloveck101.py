44520ef710cc8e6e3d3700b1193d71f2187d7d95
David <davidyen1124@gmail.com>
2013-12-05 15:04:06 +0800

    Update codes

--- iloveck101/iloveck101.py
+++ iloveck101/iloveck101.py
@@ -2,10 +2,17 @@ import os
 import sys
 import re
 
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
+
 import requests
 from lxml import etree
 from utils import get_image_info
 
+
+REQUEST_HEADERS = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
 BASE_URL = 'http://ck101.com/'
 
 
@@ -30,7 +37,7 @@ def retrieveThreadList(url):
     The url may contains many thread links. We parse them out.
     """
 
-    resp = requests.get(url)
+    resp = requests.get(url, headers=REQUEST_HEADERS)
 
     # parse html
     html = etree.HTML(resp.content)
@@ -70,7 +77,7 @@ def retrieveThread(url):
 
     # fetch html and find images
     for attemp in range(3):
-        resp = requests.get(url)
+        resp = requests.get(url, headers=REQUEST_HEADERS)
         if resp.status_code != 200:
             print 'Retrying ...'
             continue
@@ -93,25 +100,30 @@ def retrieveThread(url):
 
     # iterate and save images
     image_urls = html.xpath('//img/@file')
-    for image_url in image_urls:
+
+    def process_image_worker(image_url):
         filename = image_url.rsplit('/', 1)[1]
 
         # ignore useless image
         if not image_url.startswith('http'):
-            continue
+            return
 
         # fetch image
         print 'Fetching %s ...' % image_url
-        resp = requests.get(image_url)
+        resp = requests.get(image_url, headers=REQUEST_HEADERS)
 
         # ignore small images
         content_type, width, height = get_image_info(resp.content)
         if width < 400 or height < 400:
-            continue
+            return
 
         # save image
         with open(os.path.join(folder, filename), 'wb+') as f:
             f.write(resp.content)
+
+    jobs = [gevent.spawn(process_image_worker, image_url)
+            for image_url in image_urls]
+    gevent.joinall(jobs)
 
 
 def main():
