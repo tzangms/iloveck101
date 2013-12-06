98bd0054e63434d4f9b8d88de4c3f5534c8acb81
tzangms <tzangms@gmail.com>
2013-12-06 17:07:39 +0800

    use HTTMock instead of fetching real image

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,21 +9,31 @@ from httmock import urlmatch, HTTMock
 
 TEST_DIR = os.path.abspath(os.path.join(__file__, '..'))
 
+@urlmatch(netloc=r'(.*\.)?imgs\.cc$')
+def image_mock(url, request):
+    image = os.path.join('fixtures', 'jYY7oMF.jpg')
+    with open(os.path.join(TEST_DIR, image)) as f:
+        content = f.read()
+
+    return content
+
+
+@urlmatch(netloc=r'(.*\.)?ck101\.com$')
+def html_mock(url, request):
+    html = os.path.join('fixtures', '2818521.html')
+    with open(os.path.join(TEST_DIR, html)) as f:
+        content = f.read()
+
+    return content
+
+
 class CK101Test(unittest.TestCase):
     def setUp(self):
         self.url = 'http://ck101.com/thread-2818521-1-1.html'
 
     def test_iloveck101(self):
 
-        @urlmatch(netloc=r'(.*\.)?ck101\.com$')
-        def ck101_mock(url, request):
-            html = os.path.join('fixtures', '2818521.html')
-            with open(os.path.join(TEST_DIR, html)) as f:
-                content = f.read()
-
-            return content
-
-        with HTTMock(ck101_mock):
+        with HTTMock(html_mock, image_mock):
             iloveck101(self.url)
         
 
@@ -33,7 +43,9 @@ class UtilsTest(unittest.TestCase):
 
 
     def test_parse_url(self):
-        title, image_urls = parse_url(self.url)
+        with HTTMock(html_mock):
+            title, image_urls = parse_url(self.url)
+
         self.assertEqual(title, u'蔡依林+心凌的綜合體？！19歲「無名時代的正妹」陳敬宣現在正翻了......')
         self.assertEqual(len(image_urls), 39)
 
